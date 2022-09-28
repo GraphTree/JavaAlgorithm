ba0409fda98ea52519afa6b202545b3e5f91a27c
GraphTree <graphnodesed@gmail.com>
2022-09-28 11:41:11 +0900

    find the first and second largest number in the array

---
+++ src/main/java/BestScore.java
@@ -0,0 +1,23 @@
+// find the first and second largest number in the array
+// collections.reverseorder takes only integer class. thus do not use int[]
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BestScore {
+
+    public static String firstSecond(Integer[] myArray){
+        Integer[] arr = myArray;
+        Arrays.sort(arr, Collections.reverseOrder());
+        int first = arr[0];
+        Integer second = null;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] != first) {
+                second = arr[i];
+                break;
+            }
+        }
+
+
+    }
+}
