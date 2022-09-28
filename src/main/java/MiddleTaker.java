b69d3ad9b9baaade56066bbbaa7c630e42f63874
GraphTree <graphnodesed@gmail.com>
2022-09-28 11:28:29 +0900

    question : create an array of which last and first element are deleted

---
+++ src/main/java/MiddleTaker.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class MiddleTaker {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] middleTaker(int[] arr){
+        return Arrays.copyOfRange(arr, 1, arr.length-1)
+\    }
+}
